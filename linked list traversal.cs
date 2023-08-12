dec85ca647fd38e74a568fbb7238701a54404c98
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-08-12 20:17:08 +0530

    Create linked list traversal.cs

---
+++ linked list traversal.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using System.Xml.Schema;
+
+namespace nmew
+{
+    
+    class Node
+    {
+        public int data;
+        public Node next;
+    }
+    class Program
+    {
+
+        static void LinkedListTraversal(Node ptr)
+        {
+            while (ptr != null)
+            {
+                Console.WriteLine($"Element: {ptr.data}");
+                ptr = ptr.next;
+            }
+        } 
+        public static void Main(string[] args)
+        {
+            Node head = new Node();
+            Node second = new Node();
+            Node third = new Node();
+            Node fourth = new Node();
+
+            // linking first and second nodes
+            head.data = 7;
+            head.next = second;
+            // linking other nodes sequentially
+            second.data = 11;
+            second.next = third;
+
+            third.data = 41;
+            third.next = fourth;
+
+            fourth.data = 66;
+            fourth.next = null;
+
+            LinkedListTraversal(head);
+
+        }
+        
+    }
+}
