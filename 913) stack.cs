63baafd2e62899c9530832a7f315867e01730090
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-09-01 19:52:20 +0530

    Update 913) stack.cs

--- 913) stack.cs
+++ 913) stack.cs
@@ -1,1 +1,49 @@
+using System;
+using System.Collections.Generic;
 
+
+namespace nmew
+{
+
+   
+    class Program
+    {
+
+      
+
+        public static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);  
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\n");
+            int itempoped1 = stack.Pop();
+            int itempoped2 = stack.Pop();
+
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"item poped = {itempoped1}");
+            Console.WriteLine($"item poped = {itempoped2}");
+            Console.WriteLine("\n");
+
+            int itempeeked = stack.Peek();
+            Console.WriteLine($"item peeked = {itempeeked}");
+            
+        }
+
+    }
+}
