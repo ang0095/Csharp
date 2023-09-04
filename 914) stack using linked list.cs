3eda4a49caf5d1426f32778cdc44bb154dff2919
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-09-04 22:11:02 +0530

    Update 914) stack using linked list.cs

--- 914) stack using linked list.cs
+++ 914) stack using linked list.cs
@@ -7,7 +7,18 @@ namespace mnew
   {
     public static void Main(string[] args) 
     {
-    
+     LinkedStack stack = new LinkedStack();
+            stack.Push(78);
+            stack.Push(5);
+            stack.Push(8);
+
+            stack.LinkedListTraversal();
+            int itemPoped = stack.Pop();
+            Console.WriteLine($"Poped element: {itemPoped}");
+            stack.LinkedListTraversal();
+            Console.WriteLine("\n");
+            int itempeeked = stack.Peek();
+            Console.WriteLine(itempeeked );
     }    
   }
 }
