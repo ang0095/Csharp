6f89f910896357f4c321656663ce6bc99b908190
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-09-05 23:22:39 +0530

    Update 916) multiple parentheses matching.cs

--- 916) multiple parentheses matching.cs
+++ 916) multiple parentheses matching.cs
@@ -1,1 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
+namespace mnew
+{
+    
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+
+            string sentence = "((3+2)=5)()";
+            int x = sentence.Length;
+            Console.WriteLine(x);
+            char[] data = new char[x];
+            for (int i = 0; i < x; i++)
+            {
+                data[i] = sentence[i];
+            }
+            foreach (char element in data)
+            {
+                Console.WriteLine(element);
+            }
+            Stack<char> stack = new Stack<char>();
+            for(int i = 0; i < x; i++)
+            {
+                if (data[i] == '(') 
+                {
+                    stack.Push('(');
+                }
+                if (data[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("sequence error"); // There's a closing parenthesis without a matching opening parenthesis
+                    }
+
+                    stack.Pop();
+                }
+            }
+            if(stack.Count > 0)
+            {
+                Console.WriteLine("Unbalanced expression, extra closing brackets needed");
+            }
+            else if(stack.Count == 0) 
+            {
+                Console.WriteLine("Balanced expression");
+            }
+            else
+            {
+                Console.WriteLine("Unbalanced equation, opening bracket(s) missing");
+            }
+        }
+    }
+}
