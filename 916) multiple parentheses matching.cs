a3c9acd66852311464e8aac99cf900f8cfe1b1a7
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-09-07 19:12:54 +0530

    Update 916) multiple parentheses matching.cs

--- 916) multiple parentheses matching.cs
+++ 916) multiple parentheses matching.cs
@@ -1,26 +1,25 @@
-// This is a provided code. Check code & provide a reviewed code
 using System;
 using System.Collections.Generic;
 
-namespace BalancedParentheses
+namespace nmew
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static bool AreMatchingParentheses(char opening, char closing)
         {
-            string sentence = "((3+2)=5)[]{()}";
-            bool isBalanced = CheckBalancedParentheses(sentence);
-
-            if (isBalanced)
-            {
-                Console.WriteLine("Balanced expression");
-            }
-            else
-            {
-                Console.WriteLine("Unbalanced expression");
-            }
+            return (opening == '(' && closing == ')') ||
+                   (opening == '[' && closing == ']') ||
+                   (opening == '{' && closing == '}');
+        }
+        public static bool IsOpeningParenthesis(char c)
+        {
+            return c == '(' || c == '[' || c == '{';
         }
 
+        public static bool IsClosingParenthesis(char c)
+        {
+            return c == ')' || c == ']' || c == '}';
+        }
         public static bool CheckBalancedParentheses(string input)
         {
             Stack<char> stack = new Stack<char>();
@@ -34,30 +33,30 @@ namespace BalancedParentheses
                 else if (IsClosingParenthesis(character))
                 {
                     if (stack.Count == 0 || !AreMatchingParentheses(stack.Pop(), character))
+                    // here stack.Count == 0 takes care of condition if there's a closing parenthesis first without a matching opening parenthesis
+                    // whereas !AreMatchingParentheses(stack.Pop(), character) takes care of condition where the parentheses are not of the same type.
                     {
                         return false;
-                    }
+                    }                    
                 }
             }
 
             return stack.Count == 0;
         }
 
-        public static bool IsOpeningParenthesis(char c)
+        public static void Main(string[] args)
         {
-            return c == '(' || c == '[' || c == '{';
-        }
+            string sentence = "())(";
+            bool isBalanced = CheckBalancedParentheses(sentence);
 
-        public static bool IsClosingParenthesis(char c)
-        {
-            return c == ')' || c == ']' || c == '}';
-        }
-
-        public static bool AreMatchingParentheses(char opening, char closing)
-        {
-            return (opening == '(' && closing == ')') ||
-                   (opening == '[' && closing == ']') ||
-                   (opening == '{' && closing == '}');
+            if (isBalanced)
+            {
+                Console.WriteLine("Balanced expression");
+            }
+            else
+            {
+                Console.WriteLine("Unbalanced expression");
+            }
         }
     }
 }
