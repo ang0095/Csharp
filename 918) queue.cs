44567cae6d49fb034644d3d1f13c8015e80ac9d7
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-09-11 22:55:29 +0530

    Update 918) queue.cs

--- 918) queue.cs
+++ 918) queue.cs
@@ -1,1 +1,26 @@
+using System;
+using System.Collections.Generic;
 
+
+
+namespace nmew
+{
+   class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            
+            Queue<int> queue = new Queue<int>();    
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            foreach (int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
