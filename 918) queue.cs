484b993c857fe9cad0ef86a6d535e014e7fa972a
Anand Ghuge <109163598+ang0095@users.noreply.github.com>
2023-09-12 19:38:34 +0530

    Update 918) queue.cs

--- 918) queue.cs
+++ 918) queue.cs
@@ -11,7 +11,7 @@ namespace nmew
         public static void Main(string[] args)
         {
             
-            Queue<int> queue = new Queue<int>();    
+            Queue<int> queue = new Queue<int>();    // Queue<int> queue = new Queue<int>(new[] {1,2,3}); ------> use this for faster one line code for adding elements in queue
             queue.Enqueue(1);
             queue.Enqueue(2);
             queue.Enqueue(3);
